41d45546c392cb4722c1f38b75b74790ed74210d
Vormamim <dean.groom@det.nsw.edu.au>
2025-06-06 13:19:50 +1000

    added directory demo

--- pathDemo.py
+++ pathDemo.py
@@ -2,17 +2,8 @@ import pandas as pd
 
 # Demo: Reading a CSV file using different paths
 
-# 1. Relative path (file is in the same folder as this script)
-df_relative = pd.read_csv('dummy_data.csv')
-print("Relative path (same folder):")
+# 1. Relative path (file is in the 'data' folder inside this script's directory)
+# Ensure 'sample_data.csv' exists in the 'data' folder
+df_relative = pd.read_csv('data/sub_directory/dummy_data.csv')
+print("Relative path (data folder):")
 print(df_relative.head())
-
-# 2. Relative path to a subfolder (uncomment if your file is in a subfolder called 'data')
-# df_subfolder = pd.read_csv('data/dummy_data.csv')
-# print("\nRelative path (subfolder):")
-# print(df_subfolder.head())
-
-# 3. Absolute path (update this to your actual file location)
-# df_absolute = pd.read_csv(r'C:\Users\dgroom2\OneDrive - NSW Department of Education\Documents\GitHub\Y92025DataScience\dummy_data.csv')
-# print("\nAbsolute path:")
-# print(df_absolute.head())
